61c66fc9204476a28a3bb6c4f4d361e24d9dd988
Saujanya Srivastava <154549622+icequeen3104@users.noreply.github.com>
2026-03-06 18:53:17 +0530

    Update AddtoCart.py

--- Pages/AddtoCart.py
+++ Pages/AddtoCart.py
@@ -4,20 +4,11 @@ class CartPage:
 
     def __init__(self, page: Page):
         self.page = page
-        self.add_to_cart_btn = "#add-to-cart-sauce-labs-backpack"
-        self.item_price = ".inventory_item_price"
         self.cart_items = ".inventory_item_name"
         self.cart_prices = ".inventory_item_price"
         self.cart_quantity = ".cart_quantity"
         self.remove_button = "button:has-text('Remove')"
         self.checkout_btn = "#checkout"
-
-    def click_add_to_cart_btn(self):
-        self.page.click(self.add_to_cart_btn)
-
-    def get_item_price(self):
-        price = self.page.locator(self.item_price).first.inner_text()
-        return float(price.replace("$", ""))
 
     def get_cart_items(self):
         return self.page.locator(self.cart_items).all_inner_texts()
@@ -34,3 +25,6 @@ class CartPage:
 
     def proceed_to_checkout(self):
         self.page.click(self.checkout_btn)
+
+    def is_cart_empty(self):
+        return self.page.locator(self.cart_items).count() == 0
